cb6679217d49445d5f8351b86ddb67808bb2609c
jkxruby <jkxruby@gmail.com>
2017-08-25 09:45:42 +0800

    implement image upload

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -46,7 +46,7 @@ end
 
 private
 def product_params
-  params.require(:product).permit(:title, :description, :price, :quantity)
+  params.require(:product).permit(:title, :description, :price, :quantity, :image)
 end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,6 @@ class Product < ApplicationRecord
 
   belongs_to :user, required: false
   validates :title, presence: true
+
+  mount_uploader :image, ImageUploader
 end
