2b1bdb880d8705f343f3ac572571168e11717547
jkxruby <jkxruby@gmail.com>
2017-08-29 15:11:37 +0800

    add admin/orders/state_option button

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,7 +2,7 @@ class Admin::OrdersController < ApplicationController
   layout "admin"
 
   before_action :authenticate_user!
-  before_action :admin_required
+  before_action :require_is_admin
 
   def index
     @orders = Order.order("id DESC")
